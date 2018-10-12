e070b276d3d29b37a228ab392b4704c84db77700
Pavel Shidlovsky <p_shidlovsky@mail.ru>
2018-10-12 15:06:55 +0300

    new fields

--- ordersrequest.php
+++ ordersrequest.php
@@ -16,14 +16,16 @@
         'country_to' => 'Страна получателя (text)',
         'zip_code_to' => 'Почтовый индекс получателя (text)',
         'type' => '4_air_freight_door_to_airport',
-        'insurance' => 1,
-        'insurance_price' => 10.2,
-        'items_count' => 3,
-        'items_totalweight' => 34,
-        'items_type' => 1,
-        'items_params' => '3-5-7',
-        'items_price' => 342,
-        'client_comment' => 'Комментарий (text)',
+        'insurance'=>1,
+        'insurance_price'=>10.2,
+        'insurance_currency'=>'$',
+        'items_totalweight'=>34,
+        'items_price'=>342,
+        'client_comment'=>'Комментарий (text)Комментарий (text)Комментарий (text)Комментарий (text)Комментарий (text)Комментарий (text)',
+        'items' => [
+            ['client_comment'=>'Комент','items_type'=>1,'delivery_type'=>'package','items_count'=>12,'items_weight'=>44,'items_length'=>5,'items_width'=>54,'items_height'=>9,'items_price'=>49,'items_price_currency'=>'USD'],
+            ['client_comment'=>'Комент 2','items_type'=>0,'delivery_type'=>'pallet','items_count'=>120,'items_weight'=>440,'items_length'=>500,'items_width'=>540,'items_height'=>900,'items_price'=>490,'items_price_currency'=>'EUR'],
+        ],
     ];
 
     $types_list = $api->getTypesList();
